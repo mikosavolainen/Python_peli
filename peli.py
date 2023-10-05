3d8369c0cc9df6424eea52649ab17ae5ad657869
mikosavolainen <miko.markus2112@gmail.com>
2023-10-05 13:21:47 +0300

    v.1.1

--- peli.py
+++ peli.py
@@ -1,22 +1,27 @@
 import random
 import pygame
+import sys
+
+score = 0
 
 pygame.init()
 Running = True
 
-Screen = pygame.display.set_mode((400, 300), pygame.FULLSCREEN)
+Screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+ammus_color = (255, 0, 0)
 
 player_img = pygame.image.load('pelaaja.png')
 player_updated = pygame.transform.scale(player_img, (200, 200))
-
-ammus_img = pygame.image.load('ammus.png')
-ammus_updated = pygame.transform.scale(ammus_img, (200, 200))
 
 enemy_list = []
 otokka_img = pygame.image.load('otokka.png')
 otokka_updated = pygame.transform.scale(otokka_img, (200, 200))
 SPAWNENEMY = pygame.USEREVENT
 pygame.time.set_timer(SPAWNENEMY, 1000)
+
+# Määritellään pelaajan elämäpisteiden fontti
+font = pygame.font.Font(None, 36)
 
 class Player:
     def __init__(self):
@@ -25,7 +30,7 @@ class Player:
         self.height = 60
         self.width = 60
         self.playerUpdated = player_updated
-        self.can_shoot = True
+        self.health = 100
 
     def create_player(self):
         self.Playerss = pygame.Rect(self.xpos, self.ypos, self.height, self.width)
@@ -36,28 +41,30 @@ class Player:
         Screen.blit(player_updated, (self.xpos, self.ypos))
 
     def ammus(self):
-        if self.can_shoot:
             bullet = Bullet(self.xpos + self.width / 2, self.ypos)
             bullets.append(bullet)
-            self.can_shoot = False
 
 class Bullet:
     def __init__(self, xpos, ypos):
         self.xpos = xpos
         self.ypos = ypos
         self.speed = 5
-        self.bulletUpdated = ammus_updated
+        self.radius = 10
+        # Luo hitbox ammukselle
+        self.hitbox = pygame.Rect(self.xpos - self.radius, self.ypos - self.radius, self.radius * 2, self.radius * 2)
 
     def move(self):
         self.ypos -= self.speed
+        # Päivitä hitboxin sijainti
+        self.hitbox = pygame.Rect(self.xpos - self.radius, self.ypos - self.radius, self.radius * 2, self.radius * 2)
 
     def draw(self):
-        Screen.blit(ammus_updated, (self.xpos, self.ypos))
+        pygame.draw.circle(Screen, ammus_color, (int(self.xpos), int(self.ypos)), self.radius)
 
 class Enemy:
     def __init__(self):
-        self.size = random.randint(10, 40)
-        self.ypos = 0  
+        self.size = 1
+        self.ypos = 0
 
     def create_enemy(self):
         Enemy = pygame.Rect(random.randint(100, 700), 0, self.size, self.size)
@@ -65,17 +72,26 @@ class Enemy:
 
     def move(self):
         for e in enemy_list:
-            e.y += 3 
+            self.speed = random.randint(1, 5)
+            e.y += self.speed
 
     def draw(self):
         for e in enemy_list:
             Screen.blit(otokka_updated, (e.x, e.y))
+
+    def check_collision(self, player):
+        for e in enemy_list:
+            if player.Playerss.colliderect(e):
+                player.health -= 20
+                enemy_list.remove(e)
 
 player = Player()
 enemys = Enemy()
 player.create_player()
 
 bullets = []
+
+clock = pygame.time.Clock()
 
 while Running:
     Screen.fill((255, 255, 255))
@@ -94,15 +110,17 @@ while Running:
         player.xpos -= 5
     if keys[pygame.K_RIGHT]:
         player.xpos += 5
+    
     if keys[pygame.K_SPACE]:
         player.ammus()
+
     if keys[pygame.K_ESCAPE]:
          Running = False
 
     if tapahtuma.type == SPAWNENEMY:
         enemys.create_enemy()
-    
-    enemys.move()  # Move enemies downward
+
+    enemys.move()
 
     player.draw()
     enemys.draw()
@@ -111,9 +129,27 @@ while Running:
         bullet.move()
         bullet.draw()
 
-    bullets = [bullet for bullet in bullets if bullet.ypos > 0]
+        for enemy in enemy_list:
+            # Tarkista ammuksen hitboxin ja vihollisen Rectin leikkaus
+            if bullet.hitbox.colliderect(enemy):
+                bullets.remove(bullet)
+                enemy_list.remove(enemy)
+                score += 10
 
-    if len(bullets) == 0:
-        player.can_shoot = True
+    score_text = font.render(f'Pisteet: {score}', True, (0, 0, 0))
+    Screen.blit(score_text, (10, 50))
+
+    enemys.check_collision(player)
+
+    if player.health <= 0:
+        Running = False
+
+    health_text = font.render(f'Elämäpisteet: {player.health}', True, (0, 0, 0))
+    Screen.blit(health_text, (10, 10))
 
     pygame.display.update()
+
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()

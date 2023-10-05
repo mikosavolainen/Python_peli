e46e5a9a991ef39cf69a3e3fb5accf0d28d1fa42
mikosavolainen <miko.markus2112@gmail.com>
2023-10-05 15:36:04 +0300

    Dev v.2.0

--- peli.py
+++ peli.py
@@ -1,157 +1,123 @@
-import random
 import pygame
 import sys
+import random
 
-# Alustetaan pelin muuttujat
-score = 0
+# Alustetaan Pygame
 pygame.init()
-Running = True
 
-# Luodaan pelin ikkuna
-Screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+# Pelin asetukset
+leveys = 1200
+korkeus = 1000
+ruudun_koko = 32
+taustavari = (255, 255, 255)
+pelaajan_nopeus = 1
+vihollisten_nopeus = 1
+ammus_nopeus = 2
+pelaajan_elamapisteet = 100
+pisteet = 0
 
-# Määritellään värit
-ammus_color = (255, 0, 0)
+# Lataa kuvat
+pelaaja_kuva = pygame.image.load("pelaaja.png")
+pelaaja_kuva = pygame.transform.scale(pelaaja_kuva, (200, 200))
 
-# Ladataan pelaajan ja vihollisen kuvat
-player_img = pygame.image.load('pelaaja.png')
-player_updated = pygame.transform.scale(player_img, (200, 200))
+vihollinen_kuva1 = pygame.image.load("otokka.png")
+vihollinen_kuva = pygame.transform.scale(vihollinen_kuva1, (200, 200))
 
-otokka_img = pygame.image.load('otokka.png')
-otokka_updated = pygame.transform.scale(otokka_img, (200, 200))
+# Luo pelinäyttö
+naytto = pygame.display.set_mode((leveys, korkeus))
+pygame.display.set_caption("Vihollis peli")
 
-# Luodaan vihollislista ja asetetaan tapahtumankäsittelijä vihollisten luomiselle
-enemy_list = []
-SPAWNENEMY = pygame.USEREVENT
-pygame.time.set_timer(SPAWNENEMY, 1000)
+# Määritä pelaaja
+pelaaja_x = leveys // 2 - ruudun_koko // 2
+pelaaja_y = korkeus - ruudun_koko * 2
 
-# Määritellään pelaajan elämäpisteiden fontti
-font = pygame.font.Font(None, 36)
+# Määritä viholliset
+viholliset = []
+vihollinen_aika = 0
 
-# Luokka pelaajalle
-class Player:
-    def __init__(self):
-        self.ypos = 540
-        self.xpos = 325
-        self.height = 60
-        self.width = 60
-        self.playerUpdated = player_updated
-        self.health = 100
+# Määritä ammukset
+ammukset = []
 
-    def create_player(self):
-        self.Playerss = pygame.Rect(self.xpos, self.ypos, self.height, self.width)
+# Määritä ammuksen väri ja koko
+ammus_vari = (255, 0, 0)
+ammus_koko = 7
 
-    def draw(self):
-        Screen.blit(player_updated, (self.xpos, self.ypos))
+# Fontti pistenäytölle
+fontti = pygame.font.Font(None, 36)
 
-    def ammus(self):
-        bullet = Bullet(self.xpos + self.width / 2, self.ypos)
-        bullets.append(bullet)
-
-# Luokka ammukselle
-class Bullet:
-    def __init__(self, xpos, ypos):
-        self.xpos = xpos
-        self.ypos = ypos
-        self.speed = 5
-        self.radius = 10
-        self.hitbox = pygame.Rect(self.xpos - self.radius, self.ypos - self.radius, self.radius * 2, self.radius * 2)
-
-    def move(self):
-        self.ypos -= self.speed
-        self.hitbox = pygame.Rect(self.xpos - self.radius, self.ypos - self.radius, self.radius * 2, self.radius * 2)
-
-    def draw(self):
-        pygame.draw.circle(Screen, ammus_color, (int(self.xpos), int(self.ypos)), self.radius)
-
-# Luokka viholliselle
-class Enemy:
-    def __init__(self):
-        self.size = 1
-        self.ypos = 0
-
-    def create_enemy(self):
-        enemy = pygame.Rect(random.randint(100, 700), 0, self.size, self.size)
-        enemy_list.append(enemy)
-
-    def move(self):
-        for e in enemy_list:
-            self.speed = random.randint(1, 5)
-            e.y += self.speed
-
-    def draw(self):
-        for e in enemy_list:
-            Screen.blit(otokka_updated, (e.x, e.y))
-
-    def check_collision(self, player):
-        for e in enemy_list:
-            if player.Playerss.colliderect(e):
-                player.health -= 20
-                enemy_list.remove(e)
-
-# Luodaan pelaaja ja vihollinen
-player = Player()
-enemys = Enemy()
-player.create_player()
-
-# Luodaan ammukset
-bullets = []
-
-# Alustetaan pelikello
-clock = pygame.time.Clock()
-
-while Running:
-    # Käsitellään tapahtumat
+# Pelin pääsilmukka
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            Running = False
-        elif event.type == SPAWNENEMY:
-            enemys.create_enemy()
+            pygame.quit()
+            sys.exit()
 
-    # Pelaajan liikkeet
+    # Pelaajan liikkuminen
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_DOWN]:
-        player.ypos += 5
-    if keys[pygame.K_UP]:
-        player.ypos -= 5
-    if keys[pygame.K_LEFT]:
-        player.xpos -= 5
-    if keys[pygame.K_RIGHT]:
-        player.xpos += 5
-
-    # Ammuksien luonti
+    if keys[pygame.K_LEFT] and pelaaja_x > 0:
+        pelaaja_x -= pelaajan_nopeus
+    if keys[pygame.K_RIGHT] and pelaaja_x < leveys - ruudun_koko:
+        pelaaja_x += pelaajan_nopeus
+    if keys[pygame.K_UP] and pelaaja_y > 0:
+        pelaaja_y -= pelaajan_nopeus
+    if keys[pygame.K_DOWN] and pelaaja_y < korkeus - ruudun_koko:
+        pelaaja_y += pelaajan_nopeus
     if keys[pygame.K_SPACE]:
-        player.ammus()
+        ammukset.append([pelaaja_x + ruudun_koko // 2, pelaaja_y])
+    if keys[pygame.K_ESCAPE]:
+        pygame.quit()
+        sys.exit()
 
-    # Päivitetään näyttö
-    Screen.fill((255, 255, 255))
-    player.draw()
-    enemys.move()
-    enemys.draw()
+    # Luo uusi vihollinen
+    if vihollinen_aika == 0:
+        vihollinen_x = random.randint(0, leveys - ruudun_koko)
+        viholliset.append([vihollinen_x, 0])
+        vihollinen_aika = 100
 
-    for bullet in bullets:
-        bullet.move()
-        bullet.draw()
+    # Liikuta vihollisia alaspäin
+    for vihollinen in viholliset:
+        vihollinen[1] += vihollisten_nopeus
 
-    # Törmäykset
-    for enemy in enemy_list:
-        for bullet in bullets:
-            if bullet.hitbox.colliderect(enemy):
-                bullets.remove(bullet)
-                enemy_list.remove(enemy)
-                score += 10
+    # Liikuta ammuksia ylöspäin
+    for ammus in ammukset:
+        ammus[1] -= ammus_nopeus
 
-    enemys.check_collision(player)
+    # Tarkista osumat
+    for ammus in ammukset:
+        for vihollinen in viholliset:
+            if vihollinen[0] < ammus[0] < vihollinen[0] + ruudun_koko and vihollinen[1] < ammus[1] < vihollinen[1] + ruudun_koko:
+                ammukset.remove(ammus)
+                viholliset.remove(vihollinen)
+                pisteet += 5
 
-    # Näytetään pisteet ja elämäpisteet
-    score_text = font.render(f'Pisteet: {score}', True, (0, 0, 0))
-    health_text = font.render(f'Elämäpisteet: {player.health}', True, (0, 0, 0))
-    Screen.blit(score_text, (10, 50))
-    Screen.blit(health_text, (10, 10))
+    # Tarkista pelaajan osumat
+    for vihollinen in viholliset:
+        if vihollinen[0] < pelaaja_x < vihollinen[0] + ruudun_koko and vihollinen[1] + ruudun_koko > pelaaja_y:
+            pelaajan_elamapisteet -= 10
+            if pelaajan_elamapisteet == 0:
+                pygame.quit()
+                sys.exit()
+            viholliset.remove(vihollinen)
+
+    # Poista viholliset, jotka ovat poistuneet näytöltä
+    viholliset = [vihollinen for vihollinen in viholliset if vihollinen[1] < korkeus]
+
+    # Päivitä vihollisten aikaa
+    if vihollinen_aika > 0:
+        vihollinen_aika -= 1
+
+    # Päivitä näyttö
+    naytto.fill(taustavari)
+    naytto.blit(pelaaja_kuva, (pelaaja_x, pelaaja_y))
+    for vihollinen in viholliset:
+        naytto.blit(vihollinen_kuva, (vihollinen[0], vihollinen[1]))
+    for ammus in ammukset:
+        pygame.draw.circle(naytto, ammus_vari, (ammus[0], ammus[1]), ammus_koko)
+    
+    # Näytä elämäpisteet ja pisteet
+    elamapiste_teksti = fontti.render(f"Elämäpisteet: {pelaajan_elamapisteet}", True, (0, 0, 0))
+    pisteet_teksti = fontti.render(f"Pisteet: {pisteet}", True, (0, 0, 0))
+    naytto.blit(elamapiste_teksti, (10, 10))
+    naytto.blit(pisteet_teksti, (10, 50))
 
     pygame.display.update()
-
-    clock.tick(60)
-
-pygame.quit()
-sys.exit()

e80caf860b62c745fccabd324c0de7406eeb6fd3
mikosavolainen <miko.markus2112@gmail.com>
2023-10-06 10:50:28 +0300

    Dev v.2.2 Ammuksien bugi korjattu

--- peli.py
+++ peli.py
@@ -2,10 +2,8 @@ import pygame
 import sys
 import random
 
-# Alustetaan Pygame
 pygame.init()
 
-# Pelin asetukset
 leveys = 1200
 korkeus = 1000
 ruudun_koko = 60
@@ -16,40 +14,31 @@ ammus_nopeus = 4
 pelaajan_elamapisteet = 100
 pisteet = 0
 
-# Lataa kuvat
 pelaaja_kuva = pygame.image.load("pelaaja.png")
 pelaaja_kuva = pygame.transform.scale(pelaaja_kuva, (200, 200))
 
 vihollinen_kuva1 = pygame.image.load("otokka.png")
 vihollinen_kuva = pygame.transform.scale(vihollinen_kuva1, (200, 200))
 
-# Luo pelinäyttö
 naytto = pygame.display.set_mode((leveys, korkeus))
 pygame.display.set_caption("Vihollis peli")
 
-# Määritä pelaaja
 pelaaja_x = leveys // 2 - ruudun_koko // 2
 pelaaja_y = korkeus - ruudun_koko * 2
 
-# Määritä viholliset
 viholliset = []
 vihollinen_aika = 0
 
-# Määritä ammukset
 ammukset = []
 
-# Määritä ammuksen väri ja koko
 ammus_vari = (255, 0, 0)
 ammus_koko = 7
 
-# Fontti pistenäytölle
 fontti = pygame.font.Font(None, 36)
 
-# Lisää pelin päivitysnopeuden rajoitus
 kello = pygame.time.Clock()
-FPS = 60  # Haluttu päivitysnopeus
+FPS = 60
 
-# Pelin pääsilmukka
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -58,7 +47,6 @@ while True:
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
             ammukset.append([pelaaja_x + ruudun_koko // 2 + 65, pelaaja_y + 60]) 
   
-    # Pelaajan liikkuminen
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and pelaaja_x > 0:
         pelaaja_x -= pelaajan_nopeus
@@ -71,31 +59,25 @@ while True:
     if keys[pygame.K_ESCAPE]:
         pygame.quit()
         sys.exit()
-    # Luo uusi vihollinen
+
     if vihollinen_aika == 0:
         vihollinen_x = random.randint(0, leveys - ruudun_koko)
         viholliset.append([vihollinen_x, 0])
         vihollinen_aika = 100
 
-    # Liikuta vihollisia alaspäin
     for vihollinen in viholliset:
         vihollinen[1] += vihollisten_nopeus
 
-    # Liikuta ammuksia ylöspäin
     for ammus in ammukset:
         ammus[1] -= ammus_nopeus
 
-    # Tarkista osumat
     for ammus in ammukset:
         for vihollinen in viholliset:
             if vihollinen[0] < ammus[0] + ammus_koko // 2 < vihollinen[0] + ruudun_koko and vihollinen[1] < ammus[1] + ammus_koko // 2 < vihollinen[1] + ruudun_koko:
-
-
                 ammukset.remove(ammus)
                 viholliset.remove(vihollinen)
                 pisteet += 5
 
-    # Tarkista pelaajan osumat
     for vihollinen in viholliset:
         if vihollinen[0] < pelaaja_x < vihollinen[0] + ruudun_koko and vihollinen[1] + ruudun_koko > pelaaja_y:
             pelaajan_elamapisteet -= 10
@@ -104,14 +86,11 @@ while True:
                 sys.exit()
             viholliset.remove(vihollinen)
 
-    # Poista viholliset, jotka ovat poistuneet näytöltä
     viholliset = [vihollinen for vihollinen in viholliset if vihollinen[1] < korkeus]
 
-    # Päivitä vihollisten aikaa
     if vihollinen_aika > 0:
         vihollinen_aika -= 1
 
-    # Päivitä näyttö
     naytto.fill(taustavari)
     naytto.blit(pelaaja_kuva, (pelaaja_x, pelaaja_y))
     for vihollinen in viholliset:
@@ -119,7 +98,6 @@ while True:
     for ammus in ammukset:
         pygame.draw.circle(naytto, ammus_vari, (ammus[0], ammus[1]), ammus_koko)
     
-    # Näytä elämäpisteet ja pisteet
     elamapiste_teksti = fontti.render(f"Elämäpisteet: {pelaajan_elamapisteet}", True, (0, 0, 0))
     pisteet_teksti = fontti.render(f"Pisteet: {pisteet}", True, (0, 0, 0))
     naytto.blit(elamapiste_teksti, (10, 10))
@@ -127,5 +105,4 @@ while True:
 
     pygame.display.update()
 
-    # Rajoita päivitysnopeus
     kello.tick(FPS)
